852ca5b560f5503cdc7657038d8cd6462f4b1419
“Alireza” <alireza.es@gmail.com>
2024-12-29 15:42:26 -0700

    feat(script): run frontend development server in parallel with Flask and Celery

--- run-with-dev.sh
+++ run-with-dev.sh
@@ -6,6 +6,10 @@ docker compose -f docker-compose-dev.yaml up -d
 
 . venv/bin/activate
 
-flask --app application/app.py run --host=0.0.0.0 --port=7091
+flask --app application/app.py run --host=0.0.0.0 --port=7091 &
 
-celery -A application.app.celery worker -l DEBUG --pool solo
+celery -A application.app.celery worker -l DEBUG --pool solo &
+
+cd frontend
+
+npm run dev &
